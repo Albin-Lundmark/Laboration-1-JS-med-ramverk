e764cf0b41299cda830bb9f5eaae10827378b115
Albin <abbelundmark@gmail.com>
2024-02-16 14:49:26 +0100

    Ändrat texten under HeroCards

--- src/components/HeroCard.vue
+++ src/components/HeroCard.vue
@@ -1,73 +1,50 @@
 <script>
 import { useHeroStore } from '../stores/heroStore'
-  export default {
-    setup() {
-      const heroStore = useHeroStore()
-        heroStore.fetchData()
+export default {
+  setup() {
+    const heroStore = useHeroStore()
+    heroStore.fetchData()
 
-        return { heroStore }
-      },
-    data: () => ({
-      search: ''
-    })
-  }
+    return { heroStore }
+  },
+  data: () => ({
+    search: ''
+  })
+}
 </script>
 
 <template>
   <v-card>
-    <v-data-iterator
-      :items="heroStore.heroes"
-      :items-per-page="3"
-      :search="search"
-    >
+    <v-data-iterator :items="heroStore.heroes" :items-per-page="3" :search="search">
       <template v-slot:header>
         <v-toolbar class="px-4">
-          <v-text-field
-            v-model="search"
-            clearable
-            density="comfortable"
-            hide-details
-            placeholder="Search .."
-            prepend-inner-icon="mdi-magnify"
-            style="max-width: 400px;"
-            variant="solo"
-          ></v-text-field>
+          <v-text-field v-model="search" clearable density="comfortable" hide-details placeholder="Search .."
+            prepend-inner-icon="mdi-magnify" style="max-width: 400px;" variant="solo"></v-text-field>
         </v-toolbar>
       </template>
 
       <template v-slot:default="{ items }">
         <v-container class="pa-2" fluid>
           <v-row dense>
-            <v-col
-              v-for="item in items"
-              :key="item.name"
-              cols="auto"
-              md="4"
-            >
+            <v-col v-for="item in items" :key="item.name" cols="auto" md="4">
               <v-card class="pb-3" border flat>
                 <v-img :src="item.raw.image.url"></v-img>
 
-                <v-list-item class="mb-2" :name="item.raw.name">
-                  <template v-slot:name>
+                <v-list-item class="mb-2" :subtitle="item.raw.biography.publisher">
+                  <template v-slot:title>
                     <strong class="text-h6 mb-2">{{ item.raw.name }}</strong>
                   </template>
                 </v-list-item>
 
 
                 <div class="d-flex justify-space-between px-4">
-                    <div class="d-flex align-center text-caption text-medium-emphasis me-1">
-                      <v-icon icon="mdi-star" start></v-icon>
+                  <div class="d-flex align-center text-caption text-medium-emphasis me-1">
+                    <v-icon icon="mdi-star" start></v-icon>
 
-                      <div class="text-truncate">{{ item.name }}</div>
-                    </div>
+                    <div class="text-truncate">{{ item.raw.biography.firstAppearance }}</div>
+                  </div>
 
-                  <v-btn
-                    border
-                    flat
-                    size="small"
-                    class="text-none"
-                    text="Read"
-                  >
+                  <v-btn border flat size="small" class="text-none" text="Read">
                   </v-btn>
                 </div>
               </v-card>
@@ -78,27 +55,15 @@ import { useHeroStore } from '../stores/heroStore'
 
       <template v-slot:footer="{ page, pageCount, prevPage, nextPage }">
         <div class="d-flex align-center justify-center pa-4">
-          <v-btn
-            :disabled="page === 1"
-            icon="mdi-arrow-left"
-            density="comfortable"
-            variant="tonal"
-            rounded
-            @click="prevPage"
-          ></v-btn>
+          <v-btn :disabled="page === 1" icon="mdi-arrow-left" density="comfortable" variant="tonal" rounded
+            @click="prevPage"></v-btn>
 
           <div class="mx-2 text-caption">
             Page {{ page }} of {{ pageCount }}
           </div>
 
-          <v-btn
-            :disabled="page >= pageCount"
-            icon="mdi-arrow-right"
-            density="comfortable"
-            variant="tonal"
-            rounded
-            @click="nextPage"
-          ></v-btn>
+          <v-btn :disabled="page >= pageCount" icon="mdi-arrow-right" density="comfortable" variant="tonal" rounded
+            @click="nextPage"></v-btn>
         </div>
       </template>
     </v-data-iterator>
@@ -106,7 +71,7 @@ import { useHeroStore } from '../stores/heroStore'
 </template>
 
 <style scoped>
-*{
+* {
   margin: 0;
   padding: 0;
 }
