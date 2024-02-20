ef404201604b6184eee7f4250ca9ca3e92e85cb0
Albin <abbelundmark@gmail.com>
2024-02-20 14:53:31 +0100

    Delat upp listan av hjältar på första sidan och lagt till computed funktioner för de

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,19 @@
 import { useHeroStore } from '../stores/heroStore'
 
 export default {
+  computed: {
+    firstColumn() {
+        const heroStore = useHeroStore()
+        let column1 = heroStore.heroes.slice(0, Math.ceil(this.heroStore.heroes.length / 2))
+        return column1
+    },
+    secondColumn() {
+        const heroStore = useHeroStore()
+        let column2 = heroStore.heroes.slice(Math.ceil(this.heroStore.heroes.length / 2))
+        return column2
+    }
+  },
     setup() {
-
         const heroStore = useHeroStore()
         heroStore.fetchData()
 
@@ -15,7 +26,7 @@ export default {
 <template>
     <h2 class="text-center">Superheroes unite!</h2>
     <v-divider></v-divider>
-    <p class="text-center mx-4">Welcome to Heroes Unveiled, your portal to the extraordinary world of superheroes. Navigate through our captivating design to discover iconic champions of justice and enigmatic anti-heroes alike.</p>
+    <p class="text-center mx-4 mt-2">Welcome to Heroes Unveiled, your portal to the extraordinary world of superheroes. Navigate through our captivating design to discover iconic champions of justice and enigmatic anti-heroes alike.</p>
 
     <p class="text-center mx-4">Uncover the origins that forged these heroes, from chance encounters to transformative moments. Witness their superhuman feats, from lightning speed to titanic strength, as they battle against villainy.</p>
 
@@ -23,14 +34,25 @@ export default {
 
     <p class="text-center mx-4">Join us at Heroes Unveiled, where legends come to life with each click.
     </p>
-    <ul v-for="hero in heroStore.heroes" :key="hero.id" class="text-center text-h5 two-column-list">
-        <li>{{ hero.name }}</li>
-    </ul>
+
+    <h2 class="text-center my-3">Here's a list of all our heroes</h2>
+    <v-divider></v-divider>
+    <div class="two-column-list mt-2">
+        <ul v-for="hero in firstColumn" :key="hero.id">
+            <li class="text-center text-h6">{{ hero.name }}</li>
+        </ul>
+        <ul v-for="hero in secondColumn" :key="hero.id">
+            <li class="text-center text-h6 mb-">{{ hero.name }}</li>
+        </ul>
+    </div>
 </template>
 
 <style scoped>
 .two-column-list {
-    column-count: 2;
-    column-gap: 1rem;
+    display: grid;
+    grid-template-columns: 50% 50%;
+    grid-auto-rows: minmax(20px, auto);
+    grid-gap: 5px;
+    place-self: center
 }
 </style>
